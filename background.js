657ee8d0360756a73c0ab3cde0eda6011dcbfa74
Jason Brown <superbunker@gmail.com>
2022-10-13 18:49:31 -0700

    revised to retro mode

--- background.js
+++ background.js
@@ -1,16 +1,3 @@
-// Copyright 2022 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setBadgeText({
@@ -38,13 +25,13 @@ chrome.action.onClicked.addListener(async (tab) => {
     if (nextState === "ON") {
       // Insert the CSS file when the user turns the extension on
       await chrome.scripting.insertCSS({
-        files: ["focus-mode.css"],
+        files: ["retro-mode.css"],
         target: { tabId: tab.id },
       });
     } else if (nextState === "OFF") {
       // Remove the CSS file when the user turns the extension off
       await chrome.scripting.removeCSS({
-        files: ["focus-mode.css"],
+        files: ["retro-mode.css"],
         target: { tabId: tab.id },
       });
     }
